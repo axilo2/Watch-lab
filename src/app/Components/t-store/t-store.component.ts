8df7df7758fe24e42da185e6c6a77318dc867ac5
axilo2 <caputom2@gmail.com>
2022-09-27 11:35:49 +0200

    add something

---
+++ src/app/Components/t-store/t-store.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-t-store',
+  templateUrl: './t-store.component.html',
+  styleUrls: ['./t-store.component.scss']
+})
+export class TStoreComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,26 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { HistoryComponent } from './Components/history/history.component';
+import { HomepageComponent } from './Components/homepage/homepage.component';
+import { NavbarComponent } from './Components/navbar/navbar.component';
+import { NewsletterComponent } from './Components/newsletter/newsletter.component';
+import { PressComponent } from './Components/press/press.component';
+import { SocialComponent } from './Components/social/social.component';
+import { TStoreComponent } from './Components/t-store/t-store.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    HistoryComponent,
+    HomepageComponent,
+    NavbarComponent,
+    NewsletterComponent,
+    PressComponent,
+    SocialComponent,
+    TStoreComponent
   ],
   imports: [
     BrowserModule
